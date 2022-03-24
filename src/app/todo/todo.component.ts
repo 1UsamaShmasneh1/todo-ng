a7aef3a7ccf593b115dd662880d8c38406f55c3a
usama shmasneh <us.05.07.91@gmail.com>
2022-03-24 20:02:28 +0200

    check3

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -4,6 +4,13 @@ import { NgForm } from '@angular/forms';
 export interface Task{
   name:string;
   isUpdated:boolean;
+  isVisible:boolean
+}
+
+enum SortOption{
+  ASC = 'asc',
+  DESC = 'desc',
+  NONE = 'none'
 }
 
 @Component({
@@ -14,12 +21,17 @@ export interface Task{
 export class TodoComponent implements OnInit {
 
   tasks:Task[]=[];
+  readonly TASKS_KEY = 'tasks';
+
+  SortEnum = SortOption;
+  sort:SortOption = SortOption.NONE;
 
   constructor() { }
   handleSubmit(addForm:NgForm){
     let newTask:Task = {
       name: addForm.value.task, 
-      isUpdated: false
+      isUpdated: false,
+      isVisible: true
     }
     this.tasks.push(newTask);
     addForm.resetForm();
@@ -40,7 +52,70 @@ export class TodoComponent implements OnInit {
     this.tasks = this.tasks.filter((task:Task) => task.name != t);
   }
 
+  handleSort(sortDirection:SortOption){
+    
+    if(sortDirection === this.sort){
+      this.sort = SortOption.NONE;
+      return;
+    }
+    
+    this.sort = sortDirection;
+    
+    switch (sortDirection) {
+      case SortOption.ASC:
+        this.tasks = this.tasks.sort((a,b) => {
+          let aLower = a.name.toLowerCase();
+          let bLower = b.name.toLowerCase();
+
+          if(aLower < bLower){
+            return -1;
+          }
+          if(aLower > bLower){
+            return 1;
+          }
+          return 0;
+        })
+        break;
+
+      case SortOption.DESC:
+        this.tasks = this.tasks.sort((a,b) => {
+          let aLower = a.name.toLowerCase();
+          let bLower = b.name.toLowerCase();
+
+          if(aLower > bLower){
+            return -1;
+          }
+          if(aLower < bLower){
+            return 1;
+          }
+          return 0;
+        })
+        break;
+
+      case SortOption.NONE:
+    
+        break;
+    
+      default:
+        break;
+    }
+  }
+
+  handleSearch(v:string){
+    this.tasks.map((task) => {
+      task.isVisible = (task.name.includes(v));
+    })
+  }
+
+  handleSave():void{
+    localStorage.setItem(this.TASKS_KEY,JSON.stringify(this.tasks));
+  }
+
   ngOnInit(): void {
+    let savedTasksJson = localStorage.getItem(this.TASKS_KEY);
+    if(savedTasksJson != null){
+      this.tasks = JSON.parse(savedTasksJson);
+    }
   }
 
 }
